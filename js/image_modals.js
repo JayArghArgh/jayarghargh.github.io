43b1c36b858797799d35484bf5b0115f836039b7
jayarghargh <jayarghargh@gmail.com>
2021-03-07 18:19:09 +1030

    updated portfolio projects with descriptions

--- js/image_modals.js
+++ js/image_modals.js
@@ -19,31 +19,43 @@ let modalImg = document.getElementById("modal_image");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 // TODO This doesn't adhere to DRY (ran out of time!)
 let img1 = document.getElementById("image_1");
+const img1_text = "<h3>Hangry!</h3><p>A website created to pair foodies with food!</p>" +
+  "<p>Hangry was built using mobile first principles, and is powered by the Edamam.com API, JavaSCript, HTML, and jQuery.</p>";
 img1.onclick = function(){
 	modal.style.display = "block";
 	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
+	captionText.innerHTML = img1_text;
 }
 
 let img2 = document.getElementById("image_2");
+const img2_text = "<h3>Coding Quiz Challenge!</h3><p>A simple quiz website which allows the site owner" +
+  " to swap in / out quiz questions.</p>" +
+  "<p>This website was constructed using HTML5 and using local storage for the users answers.</p>";
 img2.onclick = function(){
 	modal.style.display = "block";
 	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
+	captionText.innerHTML = img2_text;
 }
 
 let img3 = document.getElementById("image_3");
+const img3_text = "<h3>Weather Dashboard</h3><p>Using the Google places and the OpenWeather API's," +
+  " this dashboard shows weather forecasts for users desired locations.</p>" +
+  "<p>The last ten searched locations are stored using local storage for easy revision of searches." +
+  " The website can be switched between Celcius and Farenheit at the click of a button!</p>";
 img3.onclick = function(){
 	modal.style.display = "block";
 	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
+	captionText.innerHTML = img3_text;
 }
 
 let img4 = document.getElementById("image_4");
+const img4_text = "<h3>Eat Da Burger!</h3><p>A full stack solution to a hungry persons problem... the Burger Bucket list :P</p>" +
+  "<p>Using MySQL, an Object Relational Mapper, Express, Handlebars, HTML5, CSS, and Javascript - some might call this overkill, I call it perfection!" +
+  " A \"Mobile First\" website to keep track of what you want to eat, and when you've eaten it.</p>"
 img4.onclick = function(){
 	modal.style.display = "block";
 	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
+	captionText.innerHTML = img4_text;
 }
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
